2728b7ad72a020622342edc0703245f22e135930
greg9702 <grzegorzb9702@gmail.com>
2022-03-07 20:02:36 +0100

    Added build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME=my-kube-scheduler
+IMAGE_TAG=1.0
+SCHEDULER_DEPLOYMENT_FILE=my-scheduler-deployment.yaml
+
+echo ================================================ BUILDING BINARY ================================================
+make
+echo ================================================ BUILDING IMAGE  ================================================
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
+echo ================================================  LOADING IMAGE  ================================================
+kind load docker-image $IMAGE_NAME:$IMAGE_TAG
+echo =============================================== RUNNING SCHEDULER ===============================================
+kubectl delete -f $SCHEDULER_DEPLOYMENT_FILE
+kubectl apply -f $SCHEDULER_DEPLOYMENT_FILE
+echo =================================================== COMPLETED ===================================================
